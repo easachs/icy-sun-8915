2a2a4e0fd8cbdb7f6ef30d6a5d10a8e335c8d2aa
Eli Sachs <easachs13@gmail.com>
2022-07-25 11:49:36 -0600

    Finish extension

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,8 @@
 class Ticket < ApplicationRecord
   has_many :employee_tickets
   has_many :employees, through: :employee_tickets
+
+  def fellow_ticket_holders(employee_id)
+    employees.distinct.where.not(id: employee_id)
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Ticket, type: :model do
     it { should have_many :employee_tickets }
     it { should have_many(:employees).through :employee_tickets }
   end
+
+  describe 'instance methods' do
+    it 'has fellow ticket holders' do
+      dep = Department.create!(name: "Backend", floor: 1)
+      jack = Employee.create!(name: "Jack", level: 2, department_id: dep.id)
+      john = Employee.create!(name: "John", level: 1, department_id: dep.id)
+      jeff = Employee.create!(name: "Jeff", level: 3, department_id: dep.id)
+
+      phone = Ticket.create!(subject: "phone", age: 7)
+
+      pager = Ticket.create!(subject: "pager", age: 9)
+
+      EmployeeTicket.create!(employee_id: jack.id, ticket_id: phone.id)
+      EmployeeTicket.create!(employee_id: john.id, ticket_id: phone.id)
+
+      EmployeeTicket.create!(employee_id: john.id, ticket_id: pager.id)
+      EmployeeTicket.create!(employee_id: jeff.id, ticket_id: pager.id)
+
+      expect(phone.fellow_ticket_holders(jack.id).first).to eq(john)
+      expect(phone.fellow_ticket_holders(john.id).first).to eq(jack)
+      expect(pager.fellow_ticket_holders(john.id).first).to eq(jeff)
+      expect(pager.fellow_ticket_holders(jeff.id).first).to eq(john)
+    end
+  end
 end
